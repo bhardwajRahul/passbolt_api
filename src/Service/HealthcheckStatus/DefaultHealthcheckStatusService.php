1c18f8286dfb88c5cfdb4920c28c4b9d23cb7d96
Ishan Vyas <ishan@passbolt.com>
2026-05-14 12:01:19 +0530

    PB-51039 Healtcheck status.json to check additional component such as the cache

--- src/Service/HealthcheckStatus/DefaultHealthcheckStatusService.php
+++ src/Service/HealthcheckStatus/DefaultHealthcheckStatusService.php
@@ -17,6 +17,11 @@ declare(strict_types=1);
 
 namespace App\Service\HealthcheckStatus;
 
+use App\Utility\UuidFactory;
+use Cake\Cache\Cache;
+use Cake\Log\Log;
+use Exception;
+
 class DefaultHealthcheckStatusService implements HealthcheckStatusServiceInterface
 {
     /**
@@ -24,7 +29,32 @@ class DefaultHealthcheckStatusService implements HealthcheckStatusServiceInterfa
      */
     public function check(): bool
     {
-        // If we reach this far, most likely services(i.e. db, etc.) are up
-        return true;
+        return $this->isCacheServerUp();
+    }
+
+    /**
+     * Check if cache is able to perform basic read, write, etc. operations.
+     *
+     * @return bool
+     */
+    private function isCacheServerUp(): bool
+    {
+        /**
+         * Try to write and read the cache.
+         * The reason for reading the cache is that some cache server's (i.e. redis) proxy connection might be available (hence no exception),
+         * but cache isn't actually written because the actual server is down.
+         */
+        try {
+            $key = 'healthcheck_status_' . UuidFactory::uuid();
+            $status = Cache::write($key, 'ok');
+            $status = $status && (Cache::read($key) === 'ok');
+            $status = $status && Cache::delete($key);
+        } catch (Exception $exception) {
+            Log::error($exception->getMessage());
+
+            return false;
+        }
+
+        return $status;
     }
 }
